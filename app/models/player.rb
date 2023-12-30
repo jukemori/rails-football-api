9a2851b1778d580c49085e861e641807282540fb
Jun Ukemori <jukemori@gmail.com>
2023-12-30 22:55:07 +0900

    change player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,13 @@
 class Player < ApplicationRecord
   has_many :favorite_players, dependent: :destroy
   has_many :users, through: :favorite_players
+
+  after_destroy :delete_if_last_favorite_player
+
+  private
+
+  def delete_if_last_favorite_player
+    # Delete the player only if there are no more associated favorite players
+    self.destroy if favorite_players.empty?
+  end
 end

297d48847da75b55783a9022bd3d4d93088ec916
Jun Ukemori <jukemori@gmail.com>
2023-12-31 09:21:04 +0900

    set middleware

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,13 +1,4 @@
 class Player < ApplicationRecord
   has_many :favorite_players, dependent: :destroy
   has_many :users, through: :favorite_players
-
-  after_destroy :delete_if_last_favorite_player
-
-  private
-
-  def delete_if_last_favorite_player
-    # Delete the player only if there are no more associated favorite players
-    self.destroy if favorite_players.empty?
-  end
 end
--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,12 @@ module RailsFootballApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '/graphql', headers: :any, methods: [:post]
+      end
+    end
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
